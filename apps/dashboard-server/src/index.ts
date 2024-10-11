e3826b02ae4f9847edaacc014f0e3938918231f5
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-11 16:14:17 -0400

    chore: add tRPC in Elysia

--- apps/dashboard-server/src/index.ts
+++ apps/dashboard-server/src/index.ts
@@ -1,9 +1,23 @@
+import { trpc } from "@elysiajs/trpc";
+import { initTRPC } from "@trpc/server";
 import { Elysia } from "elysia";
+import { z } from "zod";
+
+const t = initTRPC.create();
+const p = t.procedure;
+
+const router = t.router({
+	greet: p.input(z.string()).query(({ input }) => "Hello Elysia"),
+});
+
+export type Router = typeof router;
 
 const app = new Elysia({
-  prefix: '/api',
-}).get("/", () => "Hello Elysia").listen(3000);
+	prefix: "/api",
+})
+	.use(trpc(router))
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
