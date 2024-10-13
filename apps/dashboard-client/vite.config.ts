4550be5cb2a33f71dde4863bb5247f6fbac77414
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-13 11:08:05 -0400

    feat: add vite image optimizer

--- apps/dashboard-client/vite.config.ts
+++ apps/dashboard-client/vite.config.ts
@@ -1,11 +1,12 @@
+import path from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite(), react()],
+	plugins: [TanStackRouterVite(), react(), ViteImageOptimizer({})],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
