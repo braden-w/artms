71d474938918be8107938d9df15f7eed982203fd
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-13 10:55:16 -0400

    feat: init tanstack router, remove old App.tsx

--- apps/dashboard-client/src/main.tsx
+++ apps/dashboard-client/src/main.tsx
@@ -1,10 +1,41 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { trpc } from "./utils/trpc";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const queryClient = new QueryClient();
+
+const trpcClient = trpc.createClient({
+	links: [
+		httpBatchLink({
+			url: "http://localhost:8787/trpc",
+		}),
+	],
+});
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<trpc.Provider client={trpcClient} queryClient={queryClient}>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+				</QueryClientProvider>
+			</trpc.Provider>
+		</StrictMode>,
+	);
+}
