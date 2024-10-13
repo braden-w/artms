9c883b7b2f6556a5a87cf389683e3122212f5ee0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-13 15:12:07 -0400

    feat: introspect database types

---
+++ apps/dashboard-server/src/db/schema/schema.ts
@@ -0,0 +1,233 @@
+import {
+	blob,
+	integer,
+	numeric,
+	primaryKey,
+	real,
+	sqliteTable,
+	text,
+	uniqueIndex,
+} from "drizzle-orm/sqlite-core";
+
+export const sessions = sqliteTable("sessions", {
+	id: text().primaryKey().notNull(),
+	expiresAt: integer("expires_at").notNull(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => users.id),
+	googleAccessToken: text("google_access_token").notNull(),
+	googleRefreshToken: text("google_refresh_token"),
+	googleAccessTokenExpiresAt: integer(
+		"google_access_token_expires_at",
+	).notNull(),
+	googleIdToken: text("google_id_token").notNull(),
+});
+
+export const users = sqliteTable(
+	"users",
+	{
+		id: text().primaryKey().notNull(),
+		googleEmail: text("google_email").notNull(),
+		googleId: text("google_id"),
+	},
+	(table) => {
+		return {
+			googleIdUnique: uniqueIndex("users_google_id_unique").on(table.googleId),
+		};
+	},
+);
+
+export const columns = sqliteTable("columns", {
+	name: text().primaryKey().notNull(),
+	type: text().notNull(),
+	position: real().notNull(),
+	isArray: integer("is_array").notNull(),
+	options: text().notNull(),
+	filter: text(),
+	dateDisplayFormat: text("date_display_format").notNull(),
+	shortcut: text(),
+});
+
+export const embeddings = sqliteTable("embeddings", {
+	id: text().primaryKey().notNull(),
+	pageId: text("page_id").notNull(),
+	createdAt: text("created_at").notNull(),
+	updatedAt: text("updated_at").notNull(),
+	embedding: numeric().notNull(),
+});
+
+export const pages = sqliteTable("pages", {
+	title: text(),
+	content: text(),
+	resonance: text(),
+	date: text(),
+	timezone: text(),
+	createdAt: text("created_at"),
+	updatedAt: text("updated_at"),
+	"type/book/goodreadsUrl": text("type/Book/goodreads_url"),
+	"type/article/url": text("type/Article/url"),
+	"type/project/deploymentUrl": text("type/Project/deployment_url"),
+	"type/project/githubUrl": text("type/Project/github_url"),
+	"type/gitHubRepository/url": text("type/GitHub Repository/url"),
+	"type/prompt/systemPrompt": text("type/Prompt/system_prompt"),
+	"type/prompt/userPrompt": text("type/Prompt/user_prompt"),
+	"type/restaurant/yelpUrl": text("type/Restaurant/yelp_url"),
+	"type/blog/builtWith": text("type/Blog/built_with"),
+	"type/event/description": text("type/Event/description"),
+	"type/book/link": text("type/Book/link"),
+	"type/comment/videoId": text("type/Comment/video_id"),
+	"type/excerpt/source": text("type/Excerpt/source"),
+	"type/tool/url": text("type/Tool/url"),
+	"type/tool/title": text("type/Tool/title"),
+	"type/email/subject": text("type/Email/subject"),
+	"type/video/url": text("type/Video/url"),
+	"type/restaurant/recommendedBy": text("type/Restaurant/recommended_by"),
+	"type/quote/from": text("type/Quote/from"),
+	contentDraft: text("content_draft"),
+	contentReview: text("content_review"),
+	status: text(),
+	subtitle: text(),
+	url: text(),
+	visibility: text(),
+	alias: text(),
+	on: text(),
+	type: text(),
+	postOn: text("post_on"),
+	tags: text(),
+	id: text().primaryKey().notNull(),
+	references: text(),
+});
+
+export const slugs = sqliteTable(
+	"slugs",
+	{
+		id: text().primaryKey().notNull(),
+		slugText: text("slug_text").notNull(),
+		pageId: text("page_id").notNull(),
+		timestamp: text().default("sql`(CURRENT_TIMESTAMP)`"),
+	},
+	(table) => {
+		return {
+			slugTextPageId: uniqueIndex("slugs_slug_text_page_id").on(
+				table.slugText,
+				table.pageId,
+			),
+		};
+	},
+);
+
+export const googlePhotos = sqliteTable(
+	"google_photos",
+	{
+		assetId: text("asset_id").references(() => assets.id),
+		id: text().primaryKey().notNull(),
+		description: text({ length: 1000 }),
+		productUrl: text().notNull(),
+		baseUrl: text().notNull(),
+		mimeType: text().notNull(),
+		mediaMetadata: text().notNull(),
+		contributorInfo: text(),
+		filename: text().notNull(),
+	},
+	(table) => {
+		return {
+			assetIdUnique: uniqueIndex("google_photos_asset_id_unique").on(
+				table.assetId,
+			),
+		};
+	},
+);
+
+export const releases = sqliteTable("releases", {
+	id: text().primaryKey().notNull(),
+	createdAt: text("created_at").notNull(),
+	pageId: text("page_id").notNull(),
+	fullText: text("full_text").notNull(),
+	visibility: text().notNull(),
+});
+
+export const posts = sqliteTable("posts", {
+	id: text().primaryKey().notNull(),
+	createdAt: text("created_at").notNull(),
+	releaseId: text("release_id").notNull(),
+	channelId: text("channel_id").notNull(),
+});
+
+export const recordings = sqliteTable("recordings", {
+	releaseId: text("release_id").notNull(),
+	createdAt: text("created_at").notNull(),
+	recordingDurationMs: integer("recording_duration_ms").notNull(),
+	transcript: text().notNull(),
+});
+
+export const releasesAssets = sqliteTable("releases_assets", {
+	createdAt: text("created_at").notNull(),
+	releaseId: text("release_id").notNull(),
+	assetId: text("asset_id").notNull(),
+	position: integer().notNull(),
+	startTimeMs: integer("start_time_ms").notNull(),
+	durationMs: integer("duration_ms").notNull(),
+});
+
+export const assets = sqliteTable("assets", {
+	id: text().primaryKey().notNull(),
+	name: text().notNull(),
+	extension: text().notNull(),
+	pageId: text("page_id").notNull(),
+	createdAt: text("created_at").notNull(),
+	mimeType: text("mime_type"),
+	totalDurationMs: integer("total_duration_ms").notNull(),
+});
+
+export const pagesFts = sqliteTable("pages_fts", {
+	id: numeric(),
+	title: numeric(),
+	content: numeric(),
+	pagesFts: numeric("pages_fts"),
+	rank: numeric(),
+});
+
+export const pagesFtsData = sqliteTable("pages_fts_data", {
+	id: integer().primaryKey(),
+	block: blob(),
+});
+
+export const pagesFtsIdx = sqliteTable(
+	"pages_fts_idx",
+	{
+		segid: numeric().notNull(),
+		term: numeric().notNull(),
+		pgno: numeric(),
+	},
+	(table) => {
+		return {
+			pk0: primaryKey({
+				columns: [table.segid, table.term],
+				name: "pages_fts_idx_segid_term_pk",
+			}),
+		};
+	},
+);
+
+export const pagesFtsContent = sqliteTable("pages_fts_content", {
+	id: integer().primaryKey(),
+	c0: numeric(),
+	c1: numeric(),
+	c2: numeric(),
+});
+
+export const pagesFtsDocsize = sqliteTable("pages_fts_docsize", {
+	id: integer().primaryKey(),
+	sz: blob(),
+});
+
+export const pagesFtsConfig = sqliteTable("pages_fts_config", {
+	k: numeric().primaryKey().notNull(),
+	v: numeric(),
+});
+
+export const channels = sqliteTable("channels", {
+	id: text().primaryKey().notNull(),
+	name: text().notNull(),
+	platform: text(),
+});
