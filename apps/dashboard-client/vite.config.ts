5e0f21e509e13c1c6d50305feebffc12c9878d70
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-13 11:04:46 -0400

    fix: import aliases

--- apps/dashboard-client/vite.config.ts
+++ apps/dashboard-client/vite.config.ts
@@ -1,10 +1,16 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [TanStackRouterVite(), react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	server: {
 		proxy: {
 			"/api": {
