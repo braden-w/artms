df769bae46d3e584b577e16dde4a8222cb7e8610
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-18 17:40:50 -0400

    feat: enforceUserIsAuthed middleware and protectedProcedure

--- apps/dashboard-server/src/trpc.ts
+++ apps/dashboard-server/src/trpc.ts
@@ -1,6 +1,30 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { getDb } from "./db/db";
+import type { Context } from "hono";
 
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+
+// TODO: Implement this
+const isAuthed = () => true;
+
+const enforceUserIsAuthed = t.middleware(async ({ ctx, next }) => {
+	if (!isAuthed()) {
+		throw new TRPCError({
+			code: "UNAUTHORIZED",
+			message: "User not authenticated",
+		});
+	}
+
+	const db = await getDb(ctx);
+	return next({
+		ctx: {
+			...ctx,
+			db,
+		},
+	});
+});
+
+export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
