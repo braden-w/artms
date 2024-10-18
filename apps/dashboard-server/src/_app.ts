4928931b139026177223f763975b00e07dfbcbea
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-18 17:42:28 -0400

    feat: scaffold getPagesByWhereClauseWithColumns

--- apps/dashboard-server/src/_app.ts
+++ apps/dashboard-server/src/_app.ts
@@ -1,8 +1,27 @@
 import { z } from "zod";
-import { publicProcedure, router } from "./trpc";
+import { protectedProcedure, publicProcedure, router } from "./trpc";
+import { searchSchema } from "./searchSchema";
+import { buildWhereClause } from "./conditions";
+import { sql } from "drizzle-orm";
 
 export const appRouter = router({
 	greet: publicProcedure.input(z.string().nullish()).query(({ input }) => {
 		return `Hello ${input ?? "World"}!`;
 	}),
+
+	getPagesByWhereClauseWithColumns: protectedProcedure
+		.input(searchSchema)
+		.query(async ({ input, ctx }) => {
+			const { filter, orderBy, limit, offset } = input;
+			const where = buildWhereClause(filter);
+			const pageOfPages = await ctx.db.query.pagesTable.findMany({
+				where,
+				orderBy: orderBy ? sql.raw(orderBy) : undefined,
+				limit: limit,
+				offset: offset,
+			});
+			const columnsInDb = await getAllColumnsInDb();
+			await syncOptions({ columnsInDb, pages: pageOfPages });
+			return { pageOfPages, columnsInDb };
+		}),
 });
