a9fac8cf0aff53d145cde29f7764cddfea3c3dfc
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-13 11:19:56 -0500

    feat: columnsRouter

--- apps/dashboard-server/src/routers/_app.ts
+++ apps/dashboard-server/src/routers/_app.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 import { pagesRouter } from "./pagesRouter";
+import { columnsRouter } from "./columnsRouter";
 
 export const appRouter = router({
 	greet: publicProcedure.input(z.string().nullish()).query(({ input }) => {
 		return `Hello ${input ?? "World"}!`;
 	}),
-
 	pages: pagesRouter,
+	columns: columnsRouter,
 });
