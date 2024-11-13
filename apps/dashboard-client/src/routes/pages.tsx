b3f0f9cef4411886b8614b89c0d0cec2c0ae569d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-12 21:42:34 -0500

    feat: working data fetching for pages

--- apps/dashboard-client/src/routes/pages.tsx
+++ apps/dashboard-client/src/routes/pages.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, useSearch } from "@tanstack/react-router";
 import { zodSearchValidator } from "@tanstack/router-zod-adapter";
 import { searchSchema } from "@repo/dashboard-server/searchSchema";
+import { trpc } from "@/utils/trpc";
+import { Fragment } from "react/jsx-runtime";
 
 export const Route = createFileRoute("/pages")({
 	component: Pages,
@@ -12,10 +14,24 @@ function Pages() {
 		from: "/pages",
 	});
 
+	const {
+		data: { pageOfPages, allColumns } = { pageOfPages: [], allColumns: [] },
+		isPending,
+		error,
+	} = trpc.pages.getPagesByWhereClause.useQuery({
+		filter,
+		orderBy,
+		limit,
+		offset,
+	});
+
 	return (
-		<div>
-			<h1>Pages</h1>
-			<pre>{JSON.stringify({ filter, orderBy, limit, offset }, null, 2)}</pre>
-		</div>
+		<Fragment>
+			<div>
+				<h1>Pages</h1>
+				<pre>{JSON.stringify({ filter, orderBy, limit, offset }, null, 2)}</pre>
+			</div>
+			<pre>{JSON.stringify({ pageOfPages, allColumns }, null, 2)}</pre>
+		</Fragment>
 	);
 }
