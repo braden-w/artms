7c8fff968764082572f5df006201ef3688d32849
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-21 22:06:21 -0500

    chore: fast forward drizzle migrations

--- drizzle/0001_greedy_squirrel_girl.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `google_photos` RENAME COLUMN "productUrl" TO "product_url";--> statement-breakpoint
-ALTER TABLE `google_photos` RENAME COLUMN "baseUrl" TO "base_url";--> statement-breakpoint
-ALTER TABLE `google_photos` RENAME COLUMN "mimeType" TO "mime_type";--> statement-breakpoint
-ALTER TABLE `google_photos` RENAME COLUMN "mediaMetadata" TO "media_metadata";--> statement-breakpoint
-ALTER TABLE `google_photos` RENAME COLUMN "contributorInfo" TO "contributor_info";
