927a3eb6d01963d4dd35286769adaffd48aaa1dd
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-22 11:46:46 -0500

    feat: move utils

--- apps/dashboard-client/src/lib/utils.ts
+++ apps/dashboard-client/src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const isString = (input: unknown): input is string =>
+	z.string().safeParse(input).success;
+
+export const isStringArray = (input: unknown): input is readonly string[] =>
+	z.string().array().safeParse(input).success;
+
+export const isNumber = (input: unknown): input is number =>
+	z.number().safeParse(input).success;
+
+export const isBoolean = (input: unknown): input is boolean =>
+	z.boolean().safeParse(input).success;
--- apps/dashboard-server/src/utils.ts
+++ apps/dashboard-server/src/utils.ts
@@ -12,18 +12,6 @@ const AUTOMATIC_LINK_PATTERN = `<(${PAGE_ID_PATTERN})>` as const;
 export const PAGE_LINK_PATTERN =
 	`(?:${STANDARD_LINK_PATTERN}|${AUTOMATIC_LINK_PATTERN})` as const;
 
-export const isString = (input: unknown): input is string =>
-	z.string().safeParse(input).success;
-
-export const isStringArray = (input: unknown): input is readonly string[] =>
-	z.string().array().safeParse(input).success;
-
-export const isNumber = (input: unknown): input is number =>
-	z.number().safeParse(input).success;
-
-export const isBoolean = (input: unknown): input is boolean =>
-	z.boolean().safeParse(input).success;
-
 export function getFileStemAndExtension(fileName: string) {
 	const lastDotIndex = fileName.lastIndexOf(".");
 	const fileStem =
