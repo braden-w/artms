ebb7187396f46094900e43d45761dd12324553f8
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-24 17:51:01 -0500

    chore: remove greet procedure for now

--- apps/dashboard-server/src/routers/_app.ts
+++ apps/dashboard-server/src/routers/_app.ts
@@ -1,12 +1,8 @@
-import { z } from "zod";
+import { router } from "../trpc";
 import { columnsRouter } from "./columnsRouter";
 import { pagesRouter } from "./pagesRouter";
-import { publicProcedure, router } from "../trpc";
 
 export const appRouter = router({
-	greet: publicProcedure.input(z.string().nullish()).query(({ input }) => {
-		return `Hello ${input ?? "World"}!`;
-	}),
 	pages: pagesRouter,
 	columns: columnsRouter,
 });
