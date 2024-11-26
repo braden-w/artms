cb68cae7520c6dbaac11b9e89f989b511b5fead2
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-26 16:07:23 -0500

    refactor: DEFAULT_PAGE_VALUES

--- apps/dashboard-server/src/utils.ts
+++ apps/dashboard-server/src/utils.ts
@@ -1,5 +1,9 @@
 import { customAlphabet } from "nanoid";
-import { z } from "zod";
+import type { SelectPage } from "./db/schema/pages";
+import {
+	type ColumnInDatabase,
+	COLUMNS_IN_DATABASE,
+} from "./db/COLUMNS_IN_DATABASE";
 
 const NANO_ID_CHARS = "0123456789abcdefghijklmnopqrstuvwxyz";
 const NANO_ID_LENGTH = 21;
@@ -11,3 +15,22 @@ const STANDARD_LINK_PATTERN =
 const AUTOMATIC_LINK_PATTERN = `<(${PAGE_ID_PATTERN})>` as const;
 export const PAGE_LINK_PATTERN =
 	`(?:${STANDARD_LINK_PATTERN}|${AUTOMATIC_LINK_PATTERN})` as const;
+
+const DEFAULT_PAGE_VALUES = {
+	id: nanoid(),
+	status: "Needs Scaffolding",
+	date: new Date().toISOString(),
+	timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+	created_at: new Date().toISOString(),
+	updated_at: new Date().toISOString(),
+} satisfies Partial<SelectPage>;
+
+export const generateDefaultPage = (
+	initialValues?: Partial<SelectPage>,
+): SelectPage => ({
+	...(Object.fromEntries(
+		COLUMNS_IN_DATABASE.map((column) => [column.name, null]),
+	) as Record<ColumnInDatabase["name"], null>),
+	...DEFAULT_PAGE_VALUES,
+	...initialValues,
+});
