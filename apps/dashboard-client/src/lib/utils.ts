e7d4357c6232e365aae3b0b4e8434ac0b8c34374
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-26 20:51:03 -0500

    refactor: utils with isString checkers and such

--- apps/dashboard-client/src/lib/utils.ts
+++ apps/dashboard-client/src/lib/utils.ts
@@ -1,23 +1,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
-
-export const isString = (input: unknown): input is string =>
-	z.string().safeParse(input).success;
-
-export const isStringArray = (input: unknown): input is readonly string[] =>
-	z.string().array().safeParse(input).success;
-
-export const isNumber = (input: unknown): input is number =>
-	z.number().safeParse(input).success;
-
-export const isBoolean = (input: unknown): input is boolean =>
-	z.boolean().safeParse(input).success;
-
 export function getFileStemAndExtension(fileName: string) {
 	const lastDotIndex = fileName.lastIndexOf(".");
 	const fileStem =
