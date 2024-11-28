8dcea910ed3b4faac6bde12a4b68eec6beb47166
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-11-27 22:40:29 -0500

    chore: react 19 with compiler

--- apps/dashboard-client/vite.config.ts
+++ apps/dashboard-client/vite.config.ts
@@ -1,15 +1,19 @@
 import path from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { imagetools } from "vite-imagetools";
+
+const ReactCompilerConfig = {};
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		TanStackRouterVite(),
-		react(),
+		react({
+			babel: { plugins: ["babel-plugin-react-compiler", ReactCompilerConfig] },
+		}),
 		ViteImageOptimizer({}),
 		imagetools(),
 	],
