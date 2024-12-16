459fdbc3fb4a2cb32225b720987a5cacdd611307
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-15 21:51:40 -0800

    wip feat: getEditorSelection

--- apps/dashboard-client/src/components/tip-tap/menus/SuggestionToolbar.tsx
+++ apps/dashboard-client/src/components/tip-tap/menus/SuggestionToolbar.tsx
@@ -1,26 +1,47 @@
 import { Toggle } from "@/components/ui/toggle";
 import type { PageFts } from "@repo/dashboard-server/db/schema/pages";
 import type { Editor } from "@tiptap/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FloatingToolbar } from "./FloatingToolbar";
 
 const SUGGESTION_CHAR = "[[";
 
 const suggestedPages = [
-	{ id: "1", title: "Page 1", content: "Content 1" },
-	{ id: "2", title: "Page 2", content: "Content 2" },
-];
+	{ rowid: 1, id: "1", title: "Page 1", content: "Content 1" },
+	{ rowid: 2, id: "2", title: "Page 2", content: "Content 2" },
+] satisfies PageFts[];
 
 const onPageSelect = (page: PageFts) => {
 	console.log(page);
 };
 
+const getEditorSelection = (editor: Editor) => {
+	const { $from, from, to } = editor.state.selection;
+	const isCursorSelecting = from !== to;
+	if (isCursorSelecting) return false;
+	const currentPosition = from;
+	const lineStartPos = $from.start();
+	const lineTextBeforeCursor = $from.doc.textBetween(
+		lineStartPos,
+		currentPosition,
+	);
+	const triggerCharIndex = lineTextBeforeCursor.lastIndexOf(SUGGESTION_CHAR);
+	if (triggerCharIndex === -1) return false;
+	const triggerStartPos = lineStartPos + triggerCharIndex;
+	const suggestionText = $from.doc.textBetween(
+		triggerStartPos + SUGGESTION_CHAR.length,
+		currentPosition,
+	);
+	return suggestionText;
+};
+
 export function SuggestionToolbar({ editor }: { editor: Editor }) {
+	const suggestionText = getEditorSelection(editor);
+
 	const [selectedIndex, setSelectedIndex] = useState(0);
 
 	const handleKeyDown = (event: KeyboardEvent) => {
-		if (!suggestedPages.length) return;
-
+		if (!suggestionText || !suggestedPages.length) return;
 		if (event.key === "ArrowDown" || event.key === "ArrowUp") {
 			event.preventDefault();
 			const direction = event.key === "ArrowDown" ? 1 : -1;
@@ -30,21 +51,21 @@ export function SuggestionToolbar({ editor }: { editor: Editor }) {
 			);
 		}
 
-		if (event.key === "Enter" && suggestedPages[selectedIndex]) {
+		if (event.key === "Enter") {
 			event.preventDefault();
 			const selectedPage = suggestedPages[selectedIndex];
-			onPageSelect(selectedPage);
+			if (selectedPage) onPageSelect(selectedPage);
 		}
 	};
 
 	useEffect(() => {
 		setSelectedIndex(0);
-	}, [suggestedPages]);
+	}, [suggestedPages.length]);
 
 	useEffect(() => {
 		document.addEventListener("keydown", handleKeyDown);
 		return () => document.removeEventListener("keydown", handleKeyDown);
-	});
+	}, [handleKeyDown]);
 
 	return (
 		<FloatingToolbar
